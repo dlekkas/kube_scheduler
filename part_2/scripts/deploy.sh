8ce6aab6ed866912374a59b5651858ce8b012d7f
Kaishuo Zhang <kaiszhang@student.ethz.ch>
2021-04-05 08:13:10 +0200

    Common env file.

--- part_2/scripts/deploy.sh
+++ part_2/scripts/deploy.sh
@@ -5,9 +5,7 @@ fi
 
 part=$1
 
-export KOPS_STATE_STORE=gs://cca-eth-2021-group-035-kaiszhang/
-PROJECT=$(gcloud config get-value project)
-export KOPS_FEATURE_FLAGS=AlphaAllowGCE # to unlock the GCE features
+. ./env.sh
 kops create -f ../part2${part}.yaml
 kops update cluster part2${part}.k8s.local --yes --admin
 kops validate cluster --wait 10m
