50172ae6f6faa326fb3af484b0b121d8995468d0
Dimitris Lekkas <dlekkasp@gmail.com>
2021-04-26 22:11:02 +0300

    added scripts and results for question 4.2.1

--- part_4/scripts/cluster_deploy.sh
+++ part_4/scripts/cluster_deploy.sh
@@ -7,19 +7,17 @@ GROUP_NO="035"
 PROJ_ROOT_DIR=..
 
 login_key=$HOME/.ssh/cloud-computing
-proj_id=cca-eth-2021-group-${GROUP_NO}-${ETH_ID}
+proj_id=cca-eth-2021-group-${GROUP_NO}
 
 # create a bucket in Google Cloud Storage (GCS) to store configuration only if
 # the bucket doesn't already exist
-bucket_id=gs://${proj_id}/
+bucket_id=gs://${proj_id}-${ETH_ID}/
 gsutil ls -b ${bucket_id} &> /dev/null || gsutil mb ${bucket_id}
 
 if [ ! -f ${login_key} ]; then
 	echo "Creating an ssh key to login to Kubernetes nodes..."
 	ssh-keygen -t rsa -b 4096 -f cloud-computing
 fi
-
-sed -i "s/<your-gs-bucket>/${proj_id}/g" ${PROJ_ROOT_DIR}/part3.yaml
 
 export KOPS_STATE_STORE=${bucket_id}
 export PROJECT=`gcloud config get-value project`
@@ -42,21 +40,13 @@ kops validate cluster --wait 10m
 
 kubectl get nodes -o wide
 
-cat <<EOF >install_memcached.sh
-#!/bin/bash
-sudo apt update
-sudo apt install -y memcached libmemcached-tools
-sudo systemctl status memcached
-EOF
-chmod u+x install_memcached.sh
-
 cat <<EOF >install_mcperf_dynamic.sh
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install libevent-dev libzmq3-dev git make g++ --yes
 sudo apt-get build-dep memcached --yes
 git clone https://github.com/eth-easl/memcache-perf-dynamic.git
-cd memcache-perf
+cd memcache-perf-dynamic
 make
 EOF
 chmod u+x install_mcperf_dynamic.sh
@@ -71,7 +61,28 @@ CLIENT_MEASURE_NAME=`kubectl get nodes | grep client-measure | awk '{print $1}'`
 gcloud compute ssh --ssh-key-file=${login_key} ubuntu@${CLIENT_MEASURE_NAME} \
 									 --zone=europe-west3-a --command='bash -s' < install_mcperf_dynamic.sh
 
-MEMCACHED_NAME=`kubectl get nodes | grep memcached | awk '{print $1}'`
+sleep 20
+INTERNAL_MEMCACHED_IP=`kubectl get nodes -o wide| grep memcache-server | awk '{print $6}'`
+
+cat <<EOF >install_memcached.sh
+#!/bin/bash
+sudo apt update
+sudo apt install -y memcached libmemcached-tools
+sudo systemctl status memcached
+
+# update the IP that memcached is listening on
+sudo sed -i "/^-l /c\-l ${INTERNAL_MEMCACHED_IP}" /etc/memcached.conf
+# update memcached's memory limit
+sudo sed -i "/^-m /c\-m 1024" /etc/memcached.conf
+# set a default number of memcached threads (default=1)
+echo "-t 1" | sudo tee -a /etc/memcached.conf > /dev/null
+
+# restart memcached to run with updated parameters
+sudo systemctl restart memcached
+EOF
+chmod u+x install_memcached.sh
+
+MEMCACHED_NAME=`kubectl get nodes | grep memcache-server | awk '{print $1}'`
 gcloud compute ssh --ssh-key-file=${login_key} ubuntu@${MEMCACHED_NAME} \
 									 --zone=europe-west3-a --command='bash -s' < install_memcached.sh
 
---
+++ part_4/scripts/memcache_perf.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+## Script for generating results for Question 4.2.1
+set -x
+
+login_key=$HOME/.ssh/cloud-computing
+
+PROJ_ROOT_DIR=..
+RESULTS_DIR=${PROJ_ROOT_DIR}/results/question_4_2_1
+mkdir -p ${RESULTS_DIR}
+
+n_reps=$1
+if [ "$#" -ne 1 ]; then
+	echo "Usage: ./memcache_perf.sh <n-reps>"
+	exit 1
+fi
+
+max_threads=2
+max_cores=2
+
+MEMCACHED_NAME=`kubectl get nodes | grep memcache-server | awk '{print $1}'`
+INTERNAL_MEMCACHED_IP=`kubectl get nodes -o wide | grep memcache-server | awk '{print $6}'`
+
+AGENT_NAME=`kubectl get nodes | grep client-agent | awk '{print $1}'`
+INTERNAL_AGENT_IP=`kubectl get nodes -o wide | grep client-agent | awk '{print $6}'`
+
+CLIENT_MEASURE_NAME=`kubectl get nodes | grep client-measure | awk '{print $1}'`
+
+# initiate agent
+gcloud compute ssh --ssh-key-file=${login_key} ubuntu@${AGENT_NAME} \
+	--command="./memcache-perf-dynamic/mcperf -T 16 -A > agent.dat" 2>/dev/null &
+
+# CSV that contains all the latencies of all runs and all different
+# combinations of threads and cores.
+final_csv=${RESULTS_DIR}/memcached_latencies.csv
+echo "T,C,p95,QPS,target" > ${final_csv}
+
+for C in $( seq 1 ${max_cores} ); do
+
+	for T in $( seq 1 ${max_threads} ); do
+
+		for i in $( seq 1 ${n_reps} ); do
+
+			res_dir=${RESULTS_DIR}/T${T}_C${C}/rep_${i}
+			mkdir -p ${res_dir}
+
+			# set the number of cores for memcached server
+			gcloud compute ssh --ssh-key-file=${login_key} ubuntu@${MEMCACHED_NAME} \
+				--command="sudo sed -i '/^-t /c\-t ${T}' /etc/memcached.conf; \
+									 sudo systemctl restart memcached; sleep 10; \
+									 pidof memcached | xargs sudo taskset -pc 0-$((C-1))"
+
+			# we need to load memcached with values everytime it is restarted since it
+			# only maintains data in memory and may be lost upon restarting the service
+			gcloud compute ssh --ssh-key-file=${login_key} ubuntu@${CLIENT_MEASURE_NAME} \
+				--command="./memcache-perf-dynamic/mcperf -s ${INTERNAL_MEMCACHED_IP} --loadonly"
+
+			sleep 20
+
+			measure_res=latencies.raw
+			gcloud compute ssh --ssh-key-file=${login_key} ubuntu@${CLIENT_MEASURE_NAME} \
+				--command="./memcache-perf-dynamic/mcperf -s ${INTERNAL_MEMCACHED_IP} \
+									-a ${INTERNAL_AGENT_IP} --noload -T 16 -C 4 -D 4 -Q 1000 -c 4 \
+									-t 5 --scan 5000:120000:5000 > ${measure_res}"
+
+			# copy latency results from measurement node to host machine
+			gcloud compute scp --ssh-key-file=${login_key} \
+					ubuntu@${CLIENT_MEASURE_NAME}:~/${measure_res} \
+					${res_dir}/${measure_res} >/dev/null
+
+			# format the results, keep only relevant data for our plots and store as CSV
+			awk '{print $13, $17, $18}' ${res_dir}/${measure_res} | strings  \
+					| tr ' ' ',' > ${res_dir}/latencies.csv
+
+			# add results of current combination and run to final results
+			awk -v T=${T} -v C=${C} '{ printf("%d,%d,%s\n", T, C, $0); }' \
+				<(tail -n +2 ${res_dir}/latencies.csv) >> ${final_csv}
+
+		done
+	done
+done
+
+# stop agent
+gcloud compute ssh --ssh-key-file=${login_key} ubuntu@${AGENT_NAME} \
+	--command="pkill -f mcperf"
