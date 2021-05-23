58b30e05952e3097200d643ac5df4ebb751c4b25
Kaishuo Zhang <kaiszhang@student.ethz.ch>
2021-05-23 16:14:50 +0200

    Project structure change for ccsched.

---
+++ part_4/scripts/ccsched/controller/controller.go
@@ -0,0 +1,156 @@
+package controller
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"path"
+	"strconv"
+	"strings"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
+)
+
+type Controller struct {
+	*client.Client
+}
+type CpuList []int
+
+type JobInfo struct {
+	Name    string
+	Threads int
+}
+
+func (cpuList CpuList) String() string {
+	cpuStrList := make([]string, len(cpuList))
+	for i, v := range cpuList {
+		cpuStrList[i] = strconv.Itoa(v)
+	}
+	return strings.Join(cpuStrList, ",")
+}
+
+func getStartCommand(job JobInfo) []string {
+	pkg := job.Name
+	if pkg == "splash2x-fft" {
+		pkg = "splash2x.fft"
+	}
+	return []string{"./bin/parsecmgmt", "-a", "run",
+		"-p", pkg, "-i", "native", "-n", strconv.Itoa(job.Threads)}
+}
+
+// Create a single job. After the job is created, the job is in READY state.
+func (cli *Controller) CreateSingleJob(ctx context.Context, job JobInfo) {
+	id := job.Name
+	imageName := fmt.Sprintf("anakli/parsec:%v-native-reduced", id)
+
+	out, err := cli.ImagePull(ctx, imageName, types.ImagePullOptions{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	io.Copy(os.Stdout, out)
+	command := getStartCommand(job)
+	_, err = cli.ContainerCreate(ctx, &container.Config{
+		Image: imageName,
+		Cmd:   command,
+	}, nil, nil, nil, id)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Created job", id)
+
+}
+
+// Stops and remove the jobs in the job list.
+func (cli *Controller) RemoveContainers(ctx context.Context, jobs []JobInfo) {
+	for _, job := range jobs {
+		id := job.Name
+		err := cli.ContainerRemove(ctx, id, types.ContainerRemoveOptions{Force: true})
+		if err != nil {
+			log.Printf("Error removing job %v: %v", id, err)
+		} else {
+			log.Println("Removed job", id)
+		}
+	}
+
+}
+
+func (cli *Controller) SetContainerCpuAffinity(ctx context.Context, id string, cpuList CpuList) {
+	if _, err := cli.ContainerUpdate(ctx, id, container.UpdateConfig{
+		Resources: container.Resources{
+			CpusetCpus: cpuList.String(),
+		},
+	}); err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Job %v running on cpu %v", id, cpuList)
+}
+
+func (cli *Controller) SetMemcachedCpuAffinity(cpuList CpuList) {
+	cmd := exec.Command("bash", "-c",
+		"pidof memcached | xargs sudo taskset -a -cp "+cpuList.String())
+
+	stdoutStderr, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s\n", stdoutStderr)
+}
+
+func (cli *Controller) WriteLogs(ctx context.Context, resultDir string, jobs []JobInfo) {
+	logPath := path.Join(resultDir, "logs")
+	_ = os.Mkdir(logPath, 0755)
+	for _, job := range jobs {
+		id := job.Name
+		reader, err := cli.ContainerLogs(ctx, id, types.ContainerLogsOptions{
+			ShowStdout: true,
+		})
+		if err != nil {
+			log.Printf("Error getting logs for %v: %v", id, err)
+			continue
+		}
+
+		fLog, err := os.Create(path.Join(logPath, id+".out"))
+		if err != nil {
+			log.Printf("Error creating logs file for %v: %v", id, err)
+			continue
+		}
+		defer fLog.Close()
+
+		_, err = io.Copy(fLog, reader)
+		if err != nil && err != io.EOF {
+			log.Printf("Error writing logs for %v: %v", id, err)
+			continue
+		}
+	}
+
+	infoPath := path.Join(resultDir, "info")
+	_ = os.Mkdir(infoPath, 0755)
+	for _, job := range jobs {
+		id := job.Name
+		_, info, err := cli.ContainerInspectWithRaw(ctx, id, false)
+		if err != nil {
+			log.Printf("Error getting info for %v: %v", id, err)
+			continue
+		}
+
+		fInfo, err := os.Create(path.Join(infoPath, id+".json"))
+		if err != nil {
+			log.Printf("Error creating info file for %v: %v", id, err)
+			continue
+		}
+		defer fInfo.Close()
+
+		_, err = fInfo.Write(info)
+		if err != nil {
+			log.Printf("Error writing info for %v: %v", id, err)
+			continue
+		}
+	}
+}
---
+++ part_4/scripts/ccsched/scheduler/static.go
@@ -0,0 +1,98 @@
+package scheduler
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"ethz.ch/ccsched/controller"
+	"github.com/docker/docker/api/types"
+	"github.com/shirou/gopsutil/v3/cpu"
+)
+
+type StaticJob struct {
+	controller.JobInfo
+	cpuList controller.CpuList
+}
+
+type StaticScheduler struct {
+	jobInfos      []controller.JobInfo
+	availableJobs []StaticJob
+	runningJobs   map[string]StaticJob
+	completedJobs int
+}
+
+func (scheduler *StaticScheduler) JobInfos() []controller.JobInfo {
+	return scheduler.jobInfos
+}
+
+func (scheduler *StaticScheduler) Init(ctx context.Context, cli *controller.Controller) {
+	scheduler.jobInfos = []controller.JobInfo{
+		{Name: "blackscholes", Threads: 2},
+		{Name: "ferret", Threads: 2},
+		{Name: "freqmine", Threads: 2},
+		{Name: "dedup", Threads: 1},
+		{Name: "canneal", Threads: 1},
+		{Name: "splash2x-fft", Threads: 2},
+	}
+
+	// Make all the jobs ready to run.
+	for _, job := range scheduler.jobInfos {
+		cli.CreateSingleJob(ctx, job)
+		scheduler.availableJobs = append(scheduler.availableJobs, StaticJob{JobInfo: job})
+	}
+
+	scheduler.runningJobs = make(map[string]StaticJob)
+	scheduler.completedJobs = 0
+}
+
+func (scheduler *StaticScheduler) Run(ctx context.Context, cli *controller.Controller) {
+	numJobs := len(scheduler.availableJobs)
+
+	cli.SetMemcachedCpuAffinity([]int{0, 1})
+	availableCpus := []int{2, 3}
+
+	for scheduler.completedJobs != numJobs {
+		if len(scheduler.availableJobs) > 0 {
+			// There are still jobs not running.
+			nextJob := scheduler.availableJobs[0]
+			if nextJob.Threads <= len(availableCpus) {
+				// Allocate the available cpu cores to the job.
+				cli.SetContainerCpuAffinity(ctx, nextJob.Name, availableCpus[:nextJob.Threads])
+				availableCpus = availableCpus[nextJob.Threads:]
+
+				// Start the job.
+				if err := cli.ContainerStart(ctx, nextJob.Name,
+					types.ContainerStartOptions{}); err != nil {
+					log.Fatal(err)
+				}
+				scheduler.runningJobs[nextJob.Name] = nextJob
+				scheduler.availableJobs = scheduler.availableJobs[1:]
+				log.Println("Started job", nextJob.Name)
+			}
+		}
+
+		// Block for one second to get cpu stats.
+		cpuUsage, err := cpu.Percent(time.Second, true)
+		if err != nil {
+			log.Println("Error geting cpu usage:", err)
+		} else {
+			log.Println("cpu usage: ", cpuUsage)
+		}
+
+		// Check for completed jobs.
+		for jobName, job := range scheduler.runningJobs {
+			res, err := cli.ContainerInspect(ctx, jobName)
+			if err != nil {
+				log.Fatal(err)
+			}
+			if res.State.Status == "exited" {
+				// Job has completed.
+				availableCpus = append(availableCpus, job.cpuList...)
+				scheduler.completedJobs++
+				log.Println("Completed job", jobName)
+				delete(scheduler.runningJobs, jobName)
+			}
+		}
+	}
+}
